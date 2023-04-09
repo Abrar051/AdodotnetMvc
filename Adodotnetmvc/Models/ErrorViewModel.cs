50a8d07c2dab9bb7082eb1635f72e47b286e5ac7
unknown <shahriar1410051@gmail.com>
2023-04-09 13:39:32 +0600

    Ado.net mvc

---
+++ Adodotnetmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Adodotnetmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
